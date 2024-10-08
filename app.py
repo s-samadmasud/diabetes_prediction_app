6b0af828bae4dc07e89285a855d917175438537e
Suhal Samad <samadsuhal@gmail.com>
2024-10-08 16:30:44 +0600

    updated

--- app.py
+++ app.py
@@ -19,21 +19,18 @@ def predict():
     # Get user input
     data = request.form
 
-
-
     # Assuming 'data' is a dictionary
     X = np.array([round(float(data[key]), 4) for key in data.keys()])
     X = X.reshape(1, -1)
+
     # Make prediction
     prediction = model.predict(X)[0]
 
     # Get the predicted class label and color based on prediction
-    if prediction == 0:
-        result = "No Diabetes"
-    else:
-        result = "Diabetes"
+    result = "No Diabetes" if prediction == 0 else "Diabetes"
+    color = "text-bg-danger" if prediction == 0 else "text-bg-success"  # Use consistent variable name
 
-    return render_template('result.html', prediction=result)
+    return render_template('result.html', prediction=result, color=color)
 
 if __name__ == '__main__':
     app.run(debug=True)
