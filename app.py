c37c15861351beb63f328093563986ffed57e87d
suhal samad <samadsuhal@gmail.com>
2025-04-17 14:21:06 +0600

    app.py file edited

--- app.py
+++ app.py
@@ -33,4 +33,4 @@ def predict():
     return render_template('result.html', prediction=result, color=color)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True) # Example
